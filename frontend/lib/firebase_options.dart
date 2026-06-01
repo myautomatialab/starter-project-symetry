d30548cb1a6a8d10c16d5ebe44a37bdcea74f7b0
myautomatialab <migueljaimes@automatialab.com>
2026-06-01 22:56:42 +0100

    Remove comments for Android and iOS Firebase options
    Removed comments about extracted values from Google services files.

--- frontend/lib/firebase_options.dart
+++ frontend/lib/firebase_options.dart
@@ -16,7 +16,6 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // Valores extraídos de android/app/google-services.json
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyB4hO8fzQbxb4Zy7bBgNsBev-KWuqUGf2o',
     appId: '1:560559933172:android:d074879538471a81f480a1',
@@ -25,7 +24,6 @@ class DefaultFirebaseOptions {
     storageBucket: 'tortutip-mvp.firebasestorage.app',
   );
 
-  // Valores extraídos de ios/Runner/GoogleService-Info.plist
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyCRrT0JWMSL8WVzNN8IZXTDfA1TXNaZXBA',
     appId: '1:560559933172:ios:911b0a161eff1c61f480a1',
